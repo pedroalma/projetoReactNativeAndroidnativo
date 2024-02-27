238d0c5608b80427c149e8909238bcf283d9a71c
pedroalma <pedroalma554@gmail.com>
2024-02-27 11:21:57 -0300

    projetoReactNativeAndroidnativo

---
+++ loja/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View } from "react-native";
+
+import { Parte1,Parte } from "./components/Primeiro";
+
+function App(){
+    return(
+    <View>
+        <Parte1/>
+        <Parte/>  
+    </View>
+    )
+}
+
+export default App;
---
+++ loja/src/components/Primeiro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text} from 'react-native';
+export default function Parte(){
+       return <Text>Parte</Text>  
+}
+export function Parte1(){
+    return <Text>Parte1</Text>
+}
+export function Parte2(){
+    return <Text>Parte2</Text>
+}
