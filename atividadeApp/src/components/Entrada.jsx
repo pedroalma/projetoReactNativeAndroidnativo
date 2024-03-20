d3093aed44473b4606f3c373786b21ebba5ffa39
pedroalma <pedroalma554@gmail.com>
2024-03-20 11:11:42 -0300

    novo

--- atividadeApp/src/components/Entrada.jsx
+++ atividadeApp/src/components/Entrada.jsx
@@ -2,42 +2,66 @@ import React , {useState} from "react";
 import { SafeAreaView , Image, TextInput, Text , Button, StyleSheet, View } from "react-native";
 
 export default Props =>{
-    const [nome , setnome] = useState(nome)
 
-    const text = () => {
-        setnome ('ola')
+    const [nome , setNome] = useState(nome)
+    const [text , setText] = useState(text)
+    const [senha , setSenha] = useState(senha)
+    const [aviso , setAviso] = useState(aviso)
+    const [suma,setSuma] = useState(suma)
+
+    function autenticar() {
+        if((text == 'pedro') && (senha ==  123)){
+            setSuma('Bem  vindo')
+        }else{
+            setSuma('senha ou usuario incorreto')
+        }
+    
     }
 
+    function sai(){
+        if(aviso == 'Bem  vindo'){
+            setSuma('suma da qui')
+        }else{
+            (aviso == 'senha ou usuario incorreto')
+            setSuma('suma daqui')
+        }
+    }
     return(
         <SafeAreaView style={tilo.fundo}>
+
             <Image 
             source={require('../icons/foguete.png')}
             />
-            <TextInput
 
+            <TextInput
             style={tilo.in}
             label = "Email"
-            value={text}
-            onChangeText={text => setText()}
+            onChangeText={value => setText(value)}
             maxLength={10}
             placeholder="insira seu usuario"
             keyboardType="email-address"
             
             />
+
             <TextInput
             style={tilo.in}
             secureTextEntry={true}
+            onChangeText={value => setSenha(value)}
             maxLength={10}
             placeholder="sua senha"
             />
+
             <Text style={tilo.te} >Nao tem usuario. clique aqui ?</Text>
+
             <View style={tilo.fo1}>
-            <Button color={"#708090"}title="Entrar" onPress={text}/>
+            <Button color={"#708090"}title="Entrar" onPress={autenticar}/>
             </View>
+
             <View style={tilo.fo}>
-            <Button color={"#708090"} title="sair"/>
+            <Button color={"#708090"} title="sair" onPress={sai} />
             </View>
-            <Text>{nome}</Text>
+
+            <Text style={tilo.tes}>{suma}</Text>
             
         </SafeAreaView>
     )
@@ -50,11 +74,13 @@ const tilo = StyleSheet.create(
             alignItems:"center",
         },
         in:{
+            marginTop:20,
             fontSize:20,
             backgroundColor:"#83919a",
             marginTop:5,
             borderRadius:100,
             padding:6,
+            width:210,
             height:35,
             textAlign:"center",
 
@@ -67,7 +93,9 @@ const tilo = StyleSheet.create(
             marginTop:20,
             margin:10,
         },
-       
+        tes:{
+            marginTop:10,
+        }    
         
     }
 )
